a9c28c334328b1750e0c827860823ae66076ab99
SimoneZoroddu <lenviur@gmail.com>
2026-02-05 17:21:35 +0100

    correction

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -32,24 +32,35 @@ for (let i = 0; i < teachers.length; i++) {
 console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
+
 // delete teachers[1];
 // console.log(teachers);
 teachers.splice(1, 1)
+// teachers.splice(teachers.indexOf('Ed', 1))
 console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
+const isFabioPresent = null;
 
-// for
+for (let i = 0; i < teachers.length; i++) {
+  const teacher = teachers[i]
 
-// const isFabioPresent = ;
+  if (teacher == 'Fabio') {
+    console.log('I found fabio');
+    isFabioPresent = true
+    break;
+  }
+}
+
+console.log(isFabioPresent);
 
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 
-// for
-
-
-
-// const teachersString = teachers.toString();
+// const teachersString = teachers.join(', ');
 // console.log(teachersString);
+
+const teachersString = teachers.toString();
+console.log(teachersString);
+
